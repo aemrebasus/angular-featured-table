f0c77fcff6b9d8524370c1278df6f4ca505e755a
Ahmet Emrebas <aemrebasus@gmail.com>
2020-11-12 19:38:49 -0600

    navupdated

--- projects/ae-component-document/src/lib/ae-component-document.module.ts
+++ projects/ae-component-document/src/lib/ae-component-document.module.ts
@@ -2,15 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
-import { AeComponentDocumentComponent } from './ae-component-document.component';
-import { AeComponentViewComponent } from './ae-component-view/ae-component-view.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
-import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
-import { AeTextContentComponent } from './ae-text-content/ae-text-content.component';
-import { ClipboardModule } from '@angular/cdk/clipboard';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
+
+import { AeComponentDocumentComponent } from './ae-component-document.component';
+import { AeComponentViewComponent } from './ae-component-view/ae-component-view.component';
+import { AeTextContentComponent } from './ae-text-content/ae-text-content.component';
 
 @NgModule({
   declarations: [AeComponentDocumentComponent, AeComponentViewComponent, AeTextContentComponent],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AeDynamicTableModule } from 'projects/ae-dynamic-table/src/public-api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,7 +23,6 @@ import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AeDynamicTableModule,
     AeComponentDocumentModule,
     BrowserAnimationsModule,
